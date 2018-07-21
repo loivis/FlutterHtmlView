51442d68e4d850daf55af026a2aa00a1fb59ccbc
loivis <github@kinase.wang>
2018-07-21 16:30:25 +0200

    support html global data-* attributes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,25 +23,25 @@ class _MyAppState extends State<MyApp> {
 <h4>This is heading 4</h4>
 <h5>This is heading 5</h5>
 <h6>This is heading 6</h6>
-<a href="https://flutter.io">flutter.io</a>
+<a data-link="1" href="https://flutter.io">flutter.io</a>
 <p>
     leading plain text
     <strong>leading strong</strong>
-    <img alt="Header Illustration" src="https://flutter.io/images/homepage/header-illustration.png"><span class="span">span text</span>
+    <img alt="Flutter Logo" src="https://flutter.io/images/flutter-mark-square-100.png"><span class="span">span text</span>
     <video src="https://flutter.github.io/assets-for-api-docs/videos/bee.mp4"></video>
-    <a href="https://google.com">Go Google</a>
-    <a href="mailto:address@email.domain">Mail to me</a>
     <i>trailing italic</i>
     tailing plain text
 </p>
-<p>smiple p tag in the middle</p>
-<p><img alt="Flutter Logo" src="https://flutter.io/images/flutter-mark-square-100.png"></p>
+<p>
+    <a href="https://google.com">Go Google</a>
+    <a href="mailto:address@email.domain">Mail to me</a>
+</p>
 ''';
-    // html = '<h1>This is &nbsp;heading 1</h1>';
+
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Plugin example app'),
+          title: new Text('Flutter Html View Example'),
         ),
         body: new SingleChildScrollView(
           child: new Center(
